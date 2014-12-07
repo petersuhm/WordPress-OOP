c42b92b1686fc05a0bde39bd3c68d5ed63a2e586
Peter Suhm <peter@suhm.dk>
2014-12-07 17:27:49 +0700

    add plugin file

---
+++ WordPress-OOP.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Plugin Name: WordPress OOP
+ * Plugin URI: http://petersuhm.com
+ * Description: An example plugin showcasing object-oriented practices in a WordPress context.
+ * Version: 0.0.0
+ * Author: Peter Suhm
+ * Author URI: http://petersuhm.com
+ * License: GNU GENERAL PUBLIC LICENSE
+ */
+
+?>
